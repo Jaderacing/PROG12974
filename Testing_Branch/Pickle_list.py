61e5e26c1537d03459cb567160cb9b8b4bbd9d54
Aodhan O Maille <sailing@jaderacing.ca>
2023-03-18 21:45:08 -0400

    Added pickle list

--- Testing_Branch/Pickle_list.py
+++ Testing_Branch/Pickle_list.py
@@ -1,5 +1,6 @@
-# Menu for selecting which user branch to use
-# Date v1.0 2023-03-16
+import pickle
+FILE = customers.dat
+
 
 import BankAccount
 import random
@@ -29,6 +30,8 @@ def main():
 # Administrative menu
 def admin_menu():
     pass
+    customers = load_customers()
+    save_customers(customers)
     while True:
         try:
             print(f"\n{'Menu':^20}")
@@ -42,16 +45,15 @@ def admin_menu():
             selection = int(input('Please make your selection: '))
 
             if selection == 1:
-                cust_list = create_cust(cust_list)
+                create_cust()
             elif selection == 2:
-                display_cust(cust_list)
-                # print account list
+                display_cust(customers)
             elif selection == 3:
                 pass
-                view_account()
+                view_account(customers)
             elif selection == 4:
                 pass
-                update_cust()
+                update_cust(customers)
             elif selection == 5:
                 exit()
             else:
@@ -60,6 +62,7 @@ def admin_menu():
             print('Invalid input; please once again do not enter characters!')
 
 def create_cust():
+    output_file = file.open(FILE, 'wb')
     customer_list = []
     while(True):
         try:
@@ -83,10 +86,11 @@ def create_cust():
         except ValueError:
             print('Invalid input; please try again!')
             continue
-
+        finally:
+            pickle.dump(account, output_file)
+            output_file.close()
+            print("The data was written to", FILE)
         return customer_list
-    
-
 
 def display_cust(customer_list):
     i = 1
@@ -130,83 +134,56 @@ def view_account():
     pass
     # parse list and view info specific to customer
 
-def cust_input():
-    id = int(input('Please input your six-digit ID: '))
-    # Parse list for ID and send to BankAccount.BankAccout()
-    account = BankAccount.BankAccount()
-    cust_menu(account)
+def load_customers():
+    try:
+        input_file=open(FILE, "rb")
+        cust_dct=pickle.load(input_file)
+        input_file.close()
+        
+    except IOError:
+        contact_dct={}
+        
+    return contact_dct
 
-# THIS BLOCK FOR TESTING ONLY
-###########################################################################
-###########################################################################
-import BankAccount as cust
-import random
+def look_up(customer):
+    name = input("Enter a name: ")
+    print(customer.get(name, "That name is not found."))
 
-INTEREST_MODIFIER = 1.5
-INTEREST_CUTOFF = 5000
+def change(account):
+    id = int(input("Enter an ID: "))
+    if id in account:
+        name = input("Please enter a new name for the customer (or 'ENTER' to skip): ")
+        if name.strip() == "":
+            print('Value not updated')
+        else:
+            account.setName(name)
+            print('Customer updated.')
+        base_interest = input("Please input the new BASE interest rate (or 'ENTER' to skip): ")
+        if base_interest.strip() == "":
+            print('Value not updated')
+        else:
+            try:
+                new_base_interest = float(base_interest)
+                high_interest = new_base_interest + INTEREST_MODIFIER    
+                account.setInterest(new_base_interest, high_interest)
+                print('Customer updated.')
+            except ValueError:
+                print("Invalid input. Value not updated")
+        balance = input("Please input the new balance (or 'ENTER' to skip): ")
+        if balance.strip() == "":
+            print('Value not updated')
+        else:
+            try:
+                new_balance = float(balance)
+                account.setBalance(new_balance)
+                print('Customer updated.')
+            except ValueError:
+                print("Invalid input. Not updated")
+                account[name] = update
+    else:
+        print("That name is not found.")
 
-def main():
-    id = random.randint(100000, 999999)
-    name = input('Please input customer name: ')
-    interest = float(input('Please input the BASE interest rate: '))
-    highInterest = interest + INTEREST_MODIFIER
-    cutoff = INTEREST_CUTOFF
-    balance = float(input('Please input the starting balance: '))
-    account = cust.BankAccount(name, id, interest, highInterest, cutoff, balance)
-    cust_menu(account)
-###########################################################################
-###########################################################################
-
-def cust_menu(account):
-    while True:
-        try:
-            print(f"\n{'Menu':^20}")
-            print(f"{'-'*20}")
-            print('1: Check Account Details')
-            print('2: Withdraw Money')
-            print('3: Deposit Money')
-            print('4: Monthly Interest Rate')
-            print('5: Monthly Interest Earned')
-            print('6: Exit')
-            selection = int(input('Please make your selection: '))
-
-            if selection == 1:
-                print(account)
-            elif selection == 2:
-                amount = float(input('Please enter the amount to withdraw: '))
-                withdraw(account, amount)
-            elif selection == 3:
-                amount = float(input('Please enter the amount to deposit: '))
-                deposit(account, amount)
-            elif selection == 4:
-                print(f'{account.getInterestRate():.2f}%')
-            elif selection == 5:
-                print('Your interest amount is '
-                f'{account.getMonthlyInterest():.2f}$')
-            elif selection == 6:
-                 exit()
-            else:
-                print('Invalid number; please enter a '
-                'number between 1-6!')
-        except ValueError:
-                print('Invalid input; please once again '
-                'do not enter characters!')
-
-def withdraw(account, withdraw):
-    while withdraw > account.getBalance():
-        withdraw = float(input('Insufficient funds! Input new number: '))
-    account.withdraw(withdraw)
-    print(f'{withdraw:.2f}$ withdrawn. New balance is '
-    f'{account.getBalance():.2f}$') 
-
-def deposit(account, dep_amount):
-    while dep_amount < 0:
-        float(input('Please input a positive number: '))
-    account.deposit(dep_amount)
-    print(f'{dep_amount:.2f}$ deposited. New balance is '
-    f'{account.getBalance():.2f}$')
-
-def monthlyInterest(account):
-    print(f'{account.getMonthlyInterest():.2f}$')
-
-main()
+def save_customers(mycontacts):
+    output_file=open(FILE, "wb")
+    pickle.dump(mycontacts, output_file)
+    output_file.close()
